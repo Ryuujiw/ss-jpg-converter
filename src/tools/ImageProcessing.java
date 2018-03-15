1f4b0e56e38317593ffe90e43828e3abe9dd4b88
Ryuu <rj_wng96@live.jp>
2018-03-15 23:17:59 +0800

    added writeOutput(Image image, String outputDirectory) to ImageProcessing class
    This method writes to a new file with directory choosen by user.

--- src/tools/ImageProcessing.java
+++ src/tools/ImageProcessing.java
@@ -1,6 +1,10 @@
 package tools;
 
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
+import java.io.File;
 import java.io.IOException;
 import java.util.Iterator;
 
@@ -11,7 +15,7 @@ import javax.imageio.stream.ImageInputStream;
 
 public class ImageProcessing {
 	
-	public void convertBytesToImage(byte[] byteArray) throws IOException{
+	public Image convertBytesToImage(byte[] byteArray) throws IOException{
 		ByteArrayInputStream bis = new ByteArrayInputStream(byteArray);
 		Iterator<?> readers = ImageIO.getImageReadersByFormatName("jpg");
 		
@@ -21,8 +25,23 @@ public class ImageProcessing {
         ImageInputStream iis = ImageIO.createImageInputStream(source); 
         reader.setInput(iis, true);
         ImageReadParam param = reader.getDefaultReadParam();
+        Image image = reader.read(0, param); //got an image file
+        
+        return image;
 	} //converts bytes to image 
 	
+	public void writeOutput(Image image, String outputDirectory) throws IOException{
+		//bufferedImage is the RenderedImage to be written
+		BufferedImage bufferedImage = new BufferedImage(image.getWidth(null), image.getHeight(null), BufferedImage.TYPE_INT_RGB);
+	
+		Graphics2D g2 = bufferedImage.createGraphics();
+		g2.drawImage(image, null, null);
+		
+		File imageFile = new File(outputDirectory);
+		ImageIO.write(bufferedImage, "jpg", imageFile);
+		
+		System.out.println(imageFile.getPath());
+	}
 	
 	
 }
