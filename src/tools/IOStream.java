c8961753f2a760dc5e8e727199332c3e90288b60
Ryuu <rj_wng96@live.jp>
2018-03-15 22:48:04 +0800

    Added filterBytes(File[] files) that accepts a file array.
    The function takes raw bytes from each file and then write it to a ByteArrayOutputStream. using a for loop (read until .read() returns -1). By looping the files one by one I might need to implement the proposed WrapperRemover.java inside the class as well.

--- src/tools/IOStream.java
+++ src/tools/IOStream.java
@@ -1,16 +1,42 @@
 package tools;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import ui.Main;
 
 public class IOStream {
 	
-	public byte[] filterBytes(File[] files){
+	public ByteArrayOutputStream filterBytes(File[] files) throws FileNotFoundException{
+		
+		ByteArrayOutputStream bos = null;
 		
 		for(File f: files){
+			FileInputStream fis = new FileInputStream(f);
 			//input raw bytes from file system
+			
+			bos = new ByteArrayOutputStream();
+			byte[] buf = new byte[1024];
+			
+			try{
+				for(int readNum; (readNum = fis.read(buf)) != -1;){
+					bos.write(buf, 0, readNum);
+					System.out.println("read "+readNum +" bytes,");
+				}
+			} catch (IOException e){
+				Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, e);
+			}
+			
+			//implement WrapperRemover.java
+			
 		}
 		
-		return null;
+		return bos;
 		
 	}
 }
