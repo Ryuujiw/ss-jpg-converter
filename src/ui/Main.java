60a1b1f66b1118177b3ff7ffc93451f406ef5c1f
Ryuu <rj_wng96@live.jp>
2018-03-15 23:41:57 +0800

    Updated Main.java, the program's UI
    I've choosen to use absolute layout for now. As a user myself.. I prefer to input the directory path manually instead of navigating through an UI.

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -7,8 +7,10 @@ import javax.swing.JTextArea;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.JTextField;
 
 public class Main extends JFrame {
+	private JTextField txt_outputDirectory;
 	public Main() {
 		setResizable(false);
 		getContentPane().setLayout(null);
@@ -17,33 +19,42 @@ public class Main extends JFrame {
 		lbl_title.setBounds(10, 11, 114, 14);
 		getContentPane().add(lbl_title);
 		
-		JTextArea txtrSelectFiles = new JTextArea();
-		txtrSelectFiles.setText("1. Select files and output directory. \r\n2. Click Convert. \r\n3. Done. ");
-		txtrSelectFiles.setBounds(10, 36, 308, 63);
-		getContentPane().add(txtrSelectFiles);
+		JTextArea txt_instructions = new JTextArea();
+		txt_instructions.setText("1. Select files and output directory. \r\n2. Click Convert. \r\n3. Done. ");
+		txt_instructions.setBounds(10, 36, 308, 63);
+		getContentPane().add(txt_instructions);
 		
-		JTextArea textArea = new JTextArea();
-		textArea.setBounds(10, 305, 544, 93);
-		getContentPane().add(textArea);
+		JTextArea txt_log = new JTextArea();
+		txt_log.setBounds(10, 305, 544, 93);
+		getContentPane().add(txt_log);
 		
-		JLabel lblStatus = new JLabel("Status");
-		lblStatus.setBounds(10, 280, 46, 14);
-		getContentPane().add(lblStatus);
+		JLabel lbl_status = new JLabel("Status");
+		lbl_status.setBounds(10, 280, 46, 14);
+		getContentPane().add(lbl_status);
 		
-		JButton btnBrowseFiles = new JButton("Browse Files");
-		btnBrowseFiles.setBounds(10, 115, 106, 23);
-		getContentPane().add(btnBrowseFiles);
+		JButton btn_browseFiles = new JButton("Browse Files");
+		btn_browseFiles.setBounds(10, 115, 106, 23);
+		getContentPane().add(btn_browseFiles);
 		
-		JButton btnExit = new JButton("Exit ");
-		btnExit.setBounds(465, 435, 89, 23);
-		getContentPane().add(btnExit);
+		JButton btn_exit = new JButton("Exit ");
+		btn_exit.setBounds(465, 435, 89, 23);
+		getContentPane().add(btn_exit);
 		
-		JButton btnNewButton = new JButton("Convert");
-		btnNewButton.addActionListener(new ActionListener() {
+		JButton btn_convert = new JButton("Convert");
+		btn_convert.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 			}
 		});
-		btnNewButton.setBounds(10, 246, 106, 23);
-		getContentPane().add(btnNewButton);
+		btn_convert.setBounds(10, 246, 106, 23);
+		getContentPane().add(btn_convert);
+		
+		txt_outputDirectory = new JTextField();
+		txt_outputDirectory.setBounds(10, 183, 456, 23);
+		getContentPane().add(txt_outputDirectory);
+		txt_outputDirectory.setColumns(10);
+		
+		JLabel lbl_saveTo = new JLabel("Save to.. ");
+		lbl_saveTo.setBounds(10, 163, 65, 14);
+		getContentPane().add(lbl_saveTo);
 	}
 }
